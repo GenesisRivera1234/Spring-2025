08c2cbf367074b252f398370c369c4cce30283dc
Genesis Rivera <gjr82@scarletmail.rutgers.edu>
2025-05-11 15:37:57 -0400

    Update script.js

--- transformation/js/script.js
+++ transformation/js/script.js
@@ -1,1 +1,7 @@
+// When a shape is clicked, it disappears. Click again to bring it back.
+document.querySelectorAll('.grid-item').forEach(item => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('hidden');
+  });
+});
 
