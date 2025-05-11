53803a9e201f24991c0c06babbf66b92dbe78c08
Genesis Rivera <gjr82@scarletmail.rutgers.edu>
2025-05-11 15:42:20 -0400

    Update script.js

--- transformation/js/script.js
+++ transformation/js/script.js
@@ -1,4 +1,3 @@
-// Simple effect: when clicked, grid items toggle visibility
 document.querySelectorAll('.grid-item').forEach(item => {
   item.addEventListener('click', () => {
     item.classList.toggle('hidden');
