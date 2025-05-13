00f67c8bd6eeabd578f39cf91f25c8262c547db2
Genesis Rivera <gjr82@scarletmail.rutgers.edu>
2025-05-13 15:19:29 -0400

    Update script.js

--- transformation/css/script.js
+++ transformation/css/script.js
@@ -1,6 +1,5 @@
-// JavaScript to test basic click event
 document.querySelectorAll('.grid-item').forEach(item => {
-    item.addEventListener('click', function() {
-        alert('Item clicked');  // Show alert when clicked
-    });
+  item.addEventListener('click', () => {
+    item.classList.toggle('hidden');
+  });
 });
